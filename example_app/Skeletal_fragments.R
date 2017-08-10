ee3a84b0d58613f6af15135bbf867aba163393f3
edwrdchen <edwardchen2003@gmail.com>
2017-08-10 15:22:30 -0700

    Update Skeletal_fragments.R

--- example_app/Skeletal_fragments.R
+++ example_app/Skeletal_fragments.R
@@ -11,11 +11,31 @@ rock_units$unit_name <- gsub("Gp", "", rock_units$unit_name)
 
 time_scale <- read.csv("https://paleobiodb.org/data1.2/intervals/list.txt?all_records")
 
+#for testing - delete for real application
+#nlp_data <- read.table("Stromatolite2.txt", fill = TRUE, na.strings=c("", "NA"))
+#nlp_original <- stromatolite
+#
+
 #Define functions
+sentence_no_extract <- function(dependencies) {
+  dependencies$sentence_no
+}
+
 number_extract <- function(dependencies) {
   
-  subset(dependencies["dependent"], dependencies["type"] == "nummod" & dependencies["governor"] == "%")
+  #number_only <- 
+  subset(dependencies$dependent, dependencies$type == "nummod" & dependencies$governor == "%")
   
+ # if (number_only = NA) {
+ #   return(NA)
+ # } else {
+ #   number_only
+ # }
+  
+ #sentence_no <- droplevels(subset(dependencies$sentence, dependencies["governor"]=="%" & dependencies["type"]=="nummod"))
+
+ #data.frame(sentence_no, number_only)
+
 }
 
 
@@ -53,7 +73,7 @@ time_period_extract <- function(dependencies) {
 
 
 time_extract <- function(formation_name) {
-
+  
   formation_name <- gsub("[Mm]ember", "", formation_name)
   formation_name <- gsub("[Mm]br", "", formation_name)
   formation_name <- gsub("[Mb]b", "", formation_name)
@@ -63,9 +83,9 @@ time_extract <- function(formation_name) {
   
   formation_name <- gsub("[Gg]roup", "", formation_name)
   formation_name <- gsub("[Gg]p", "", formation_name)
-
+  
   rock_time <- subset(rock_units$b_age, rock_units$unit_name == formation_name)
-
+  
   median(rock_time, na.rm = TRUE)
 }
 
@@ -96,11 +116,14 @@ NLP_Info_Extract <- function(nlp) {
     
     dependencies$governor <- dependencies$dependent[as.numeric(dependency_number)]
     
+    dependencies$sentence_no <- as.character(nlp[[2]])
+    
     dependencies
   },
   error=function(error_message){
     message("Incomplete final line found by readTableHeader on 'text'")
     message(error_message)
+
     }
   )
 }
@@ -117,7 +140,7 @@ good_sentences <- good_sentences_blank[complete.cases(good_sentences_blank),]
 
 #create data frame with governor, dependent, and relationship type
 sentence_parse <- apply(good_sentences, 1, function(x) NLP_Info_Extract(x))
-                        
+
 sentence_parsed <- sentence_parse[!sapply(sentence_parse, is.null)]
 #extracts time periods from previous data frame
 time_period <- lapply(sentence_parsed, time_period_extract)
@@ -131,20 +154,20 @@ formation_clean <- sapply(formations, function(x) paste(lapply(x, paste, collaps
 
 
 #finds skeletal abundance
-skeletal_abund <- number_extract(sentence_parsed)
+skeletal_abund <- lapply(sentence_parsed, number_extract)
 
-skeletal_abund <- subset(skeletal_abund, skeletal_abund$sentence_no %in% rownames(table(skeletal_abund$sentence_no))[which(table(skeletal_abund$sentence_no) == 1)])
-
-
+skeletal_abund <- unlist(skeletal_abund)
 #finds age of formation in millions of years
 age_data <- sapply(formation_clean, time_extract)
 
 final_results <- data.frame(formation_clean, time_period=unlist(time_period), age_data)
 
-#adds skeletal grains 
-final_results$skeletal <- skeletal_abund$number_only[match(rownames(final_results), skeletal_abund$sentence_no)]
+#Finds sentence number
+#sentence_no <- lapply(sentence_parsed, sentence_no_extract)
 
+#adds skeletal grains
+final_results$skeletal <- skeletal_abund[match(rownames(final_results), names(skeletal_abund))]
 
-subset(final_results, is.na(final_results$age_data) == F | is.na(final_results$time_period)==F)
+final1 <- subset(final_results, is.na(final_results$age_data) == F | is.na(final_results$time_period)==F)
 
-
+subset(final1, is.na(final1$skeletal)==F)
